fdab6f1456ad9871de161e816a16e4169bff969b
Kaue <kauehspacki@gmail.com>
2024-02-15 11:38:39 -0300

    Adicionado tratamento de dados das opções e opção 5 e 6 do desafio

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,7 +10,13 @@ while (true)
     op = Int32.Parse(Console.ReadLine().Trim());
     if (op == 0)
     {
+        Console.WriteLine("Encerrando...");
         System.Environment.Exit(0);
+    } else if (op != 1 && op != 2 && op != 3 && op != 4 && op != 5 && op != 6)
+    {
+        Console.WriteLine("Opção não existe!");
+        Console.ReadKey();
+        continue;
     }
     Console.Write("Primeiro número: ");
     var num1 = Int32.Parse(Console.ReadLine().Trim());
@@ -45,6 +51,22 @@ while (true)
             Console.WriteLine($"A divisão é {resultado}!!!");
             Console.ReadKey();
             break;
+        case 5:
+            if (num2 == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero!");
+                Console.ReadKey();
+                break;
+            }
+            var resto = RestoDaDivisao(num1, num2);
+            Console.WriteLine($"O resto da divisão é {resto}!!!");
+            Console.ReadKey();
+            break;
+        case 6:
+            resultado = Potenciacao(num1, num2);
+            Console.WriteLine($"A potenciação é {resultado}!!!");
+            Console.ReadKey();
+            break;
     }
 }
 
@@ -55,6 +77,8 @@ void Menu()
     Console.WriteLine("2 - Subtrair");
     Console.WriteLine("3 - Multiplicar");
     Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Resto da divisão");
+    Console.WriteLine("6 - Potenciação: base pelo expoente");
     Console.WriteLine("0 - Sair");
     Console.WriteLine("----------------------------");
 }
@@ -77,5 +101,15 @@ double Multiplicacao(int a, int b)
 double Divisao(int a, int b)
 {
     return a / b;
+}
+
+double RestoDaDivisao(int a, int b)
+{
+    return a % b;
+}
+
+double Potenciacao(int a, int b)
+{
+    return double.Pow(a, b);
 }
     
